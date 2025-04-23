de5c6b3146f08843402e03199b0e887a126cbfb8
lvinia <145567312+lvinia@users.noreply.github.com>
2025-04-23 16:24:45 -0300

    aulaDia23

--- POO.2/Assets/Scripts/Inimigo.cs
+++ POO.2/Assets/Scripts/Inimigo.cs
@@ -4,4 +4,46 @@ public class Inimigo : Personagem
 {
     private string armadura;
     private string arma;
+
+    public void AtribuirArmadura(string armadura)
+    {
+        this.armadura = armadura;
+    }
+
+    public string Armadura()
+    {
+        return this.armadura;
+    }
+
+    public void AtribuirArma(string arma)
+    {
+        this.arma = arma;
+    }
+
+    public string Arma()
+    {
+        return this.arma;
+    }
+
+
+    public int DanoDoInimigo()
+    {
+
+        int dano = 0;
+
+        switch (arma)
+        {
+            case "ESPADA":
+                dano = ForcaAtaque() + 10;
+                break;
+            case "MACHADO":
+                dano = ForcaAtaque() + 18;
+                break;
+            case "ADAGA":
+                dano = ForcaAtaque() + 5;
+                break;
+        }
+
+        return dano;
+    }
 }
--- POO.2/Assets/Scripts/Personagem.cs
+++ POO.2/Assets/Scripts/Personagem.cs
@@ -3,11 +3,79 @@ using UnityEngine;
 public class Personagem : MonoBehaviour
 {
     private string nome;
-    private float energia;
-    private float forca_ataque;
+    private int energia;
+    private int forca_ataque;
     private float forca_do_pulo;
     private float velocidade;
     private int numero_de_pes;
     private int numero_de_maos;
+
+    public void AtribuirNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public string NomeDoPersonagem()
+    {
+        return this.nome;
+    }
+    public void AtribuirEnergia(int energia)
+    {
+        this.energia = energia;
+    }
+
+    public int Energia()
+    {
+        return this.energia;
+    }
+    public void AtribuirForcaAtaque(int forca_ataque)
+    {
+        this.forca_ataque = forca_ataque;
+    }
+
+    public int ForcaAtaque()
+    {
+        return this.forca_ataque;
+    }
+    public void AtribuirForcaPulo(float forca_do_pulo)
+    {
+        this.forca_do_pulo = forca_do_pulo;
+    }
+
+    public float ForcaPulo()
+    {
+        return this.forca_do_pulo;
+    }
+    public void AtribuirVelocidade(float velocidade)
+    {
+        this.velocidade = velocidade;
+    }
+
+    public float Velocidade()
+    {
+        return this.velocidade;
+    }
+    public void AtribuirNumeroPes(int numero_de_pes)
+    {
+        this.numero_de_pes = numero_de_pes;
+    }
+
+    public int NumeroDePes()
+    {
+        return this.numero_de_pes;
+    }
+    public void AtribuirNumeroMaos(int numero_de_maos)
+    {
+        this.numero_de_maos = numero_de_maos;
+    }
+
+    public int NumeroDeMaos()
+    {
+        return this.numero_de_maos;
+    }
+    
+    
+    
+    
 
 }
