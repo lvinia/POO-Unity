225ba61717299b36325e8b03715a5dd8d2c6baf1
lvinia <145567312+lvinia@users.noreply.github.com>
2025-06-11 16:18:37 -0300

    D11M6

--- POO.2/Assets/Scripts/CameraSeguir.cs
+++ POO.2/Assets/Scripts/CameraSeguir.cs
@@ -1,16 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSeguir : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform _alvo;
+    public float interpolacao = 12f;
+    public float CamMoveSpeed = 5f;
+    public float distancia = -5;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            new Vector3(_alvo.position.x,transform.position.y,_alvo.position.z+distancia), 
+            CamMoveSpeed * Time.deltaTime);
+         
     }
 }
